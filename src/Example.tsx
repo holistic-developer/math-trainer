1587cb9dca1b60a9b56aa20291201784dc9b02cc
Andreas Taranetz <andreas.taranetz@gmail.com>
2023-01-21 18:50:01 +0100

    Change state management to useReducer

--- src/Example.tsx
+++ src/Example.tsx
@@ -1,44 +1,50 @@
-import {useState} from "react";
+import {useReducer} from "react";
 import {Answers} from "./Answers";
 import {getRandomInt, getRandomIntExcept} from "./randomNumberUtil";
 
 export const minValue = 1;
 export const maxValue = 10;
 
-function useCalculateExample() {
-    const [a, setA] = useState(getRandomInt(minValue, maxValue));
-    const [b, setB] = useState(getRandomInt(minValue, maxValue));
-    const [addition, setAddition] = useState(Boolean(getRandomInt(0, 1)));
+type ExampleState = {
+    a: number;
+    b: number;
+    addition: boolean;
+    answers: number[];
+    correct: number;
+}
 
-    const calculateExample = () => {
-        const addition = Boolean(getRandomInt(0,1));
-        const newA = getRandomIntExcept(addition ? minValue : minValue + 1, maxValue, [a]);
-        const newB = getRandomIntExcept(minValue, addition ? maxValue : a - 1, [b]);
-        setAddition(addition);
-        setA(newA);
-        setB(newB);
-    }
+function calculateExample(state: ExampleState) {
+    state.addition = Boolean(getRandomInt(0, 1));
+    state.a = getRandomIntExcept(state.addition ? minValue : minValue + 1, maxValue, [state.a]);
+    state.b = getRandomIntExcept(minValue, state.addition ? maxValue : state.a - 1, [state.b]);
 
-    const minAnswer = addition ? 2 : 1
-    const maxAnswer = addition ? maxValue * 2 : maxValue
-    const correct = addition ? a + b : a - b;
+    const minAnswer = state.addition ? 2 : 1
+    const maxAnswer = state.addition ? maxValue * 2 : maxValue
+    const correct = state.addition ? state.a + state.b : state.a - state.b;
     const answers: number[] = [];
     for (let i = 0; i < 5; i++) {
-        answers.push(getRandomIntExcept(minAnswer, maxAnswer, [... answers, correct]))
+        answers.push(getRandomIntExcept(minAnswer, maxAnswer, [...answers, correct]))
     }
     answers.splice(getRandomInt(0, 5), 0, correct);
-
-    return {a, b, addition, answers, correct, calculateExample};
+    state.answers = answers;
+    state.correct = correct;
+    return {...state};
 }
 
 export const Example: React.FC = () => {
-    const {a, b, addition, answers, correct, calculateExample} = useCalculateExample();
+    const [{a, b, addition, answers, correct}, dispatch] = useReducer(calculateExample, {
+        a: 1,
+        b: 2,
+        addition: true,
+        answers: [],
+        correct: 3
+    }, arg => calculateExample(arg));
     return (
         <main>
             <h1>{a} {addition ? '+' : '-'} {b} =</h1>
             <Answers correct={correct}
                      answers={answers}
-                     success={() => calculateExample()}/>
+                     success={dispatch}/>
         </main>
     );
 }
