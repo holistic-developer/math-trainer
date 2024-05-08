a081607be1b30100389ce9e2648b13be3d70502f
Andreas Taranetz <57600104+holistic-developer@users.noreply.github.com>
2024-05-08 17:29:35 +0000

    Fix styles

--- src/Game.tsx
+++ src/Game.tsx
@@ -31,20 +31,20 @@ export const Game: React.FC = () => {
           <section>
             {rounds} Beispiele
             <span style={{ display: 'flex', gap: '1vw', justifyContent: 'center' }}>
-              <button onClick={() => setRounds((prev) => prev + 1)}>➕</button>
-              <button onClick={() => setRounds((prev) => prev - 1)} disabled={rounds <= 2}>➖</button>
+              <button onClick={() => setRounds((prev) => prev + 1)}>+</button>
+              <button onClick={() => setRounds((prev) => prev - 1)} disabled={rounds <= 2}>-</button>
             </span>
           </section>
           <section className="game-modes">
-            <button onClick={startGame(GameMode.ADDITION_10)}>➕ bis 10</button>
-            <button onClick={startGame(GameMode.ADDITION_20)}>➕ bis 20</button>
-            <button onClick={startGame(GameMode.ADDITION_100)}>➕ bis 100</button>
-            <button onClick={startGame(GameMode.SUBTRACTION_10)}>➖ bis 10</button>
-            <button onClick={startGame(GameMode.SUBTRACTION_20)}>➖ bis 20</button>
-            <button onClick={startGame(GameMode.SUBTRACTION_100)}>➖ bis 100</button>
-            <button onClick={startGame(GameMode.ADDITION_SUBTRACTION_10)}>➕&nbsp;/&nbsp;➖&nbsp;bis&nbsp;10</button>
-            <button onClick={startGame(GameMode.ADDITION_SUBTRACTION_20)}>➕&nbsp;/&nbsp;➖&nbsp;bis&nbsp;20</button>
-            <button onClick={startGame(GameMode.ADDITION_SUBTRACTION_100)}>➕&nbsp;/&nbsp;➖&nbsp;bis&nbsp;100</button>
+            <button onClick={startGame(GameMode.ADDITION_10)}>+ bis 10</button>
+            <button onClick={startGame(GameMode.ADDITION_20)}>+ bis 20</button>
+            <button onClick={startGame(GameMode.ADDITION_100)}>+ bis 100</button>
+            <button onClick={startGame(GameMode.SUBTRACTION_10)}>- bis 10</button>
+            <button onClick={startGame(GameMode.SUBTRACTION_20)}>- bis 20</button>
+            <button onClick={startGame(GameMode.SUBTRACTION_100)}>- bis 100</button>
+            <button onClick={startGame(GameMode.ADDITION_SUBTRACTION_10)}>+&nbsp;/&nbsp;- bis&nbsp;10</button>
+            <button onClick={startGame(GameMode.ADDITION_SUBTRACTION_20)}>+&nbsp;/&nbsp;- bis&nbsp;20</button>
+            <button onClick={startGame(GameMode.ADDITION_SUBTRACTION_100)}>+&nbsp;/&nbsp;- bis&nbsp;100</button>
             <button style={{gridColumn: "span 3"}} onClick={startGame(GameMode.MULTIPLICATION_100)}>1×1 bis 100</button>
           </section>
         </>
